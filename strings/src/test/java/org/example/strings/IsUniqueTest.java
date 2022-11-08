5be2527bac17d0062aff63247276ef5991ad2beb
Nathan Carver <nathan.carver@bionautlabs.com>
2022-11-07 19:43:52 -0800

    Added empty test for Is Unique exercise.

---
+++ strings/src/test/java/org/example/strings/IsUniqueTest.java
@@ -0,0 +1,13 @@
+package org.example.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IsUniqueTest {
+
+    @Test
+    public void test() {
+        assertThat(1+1).isEqualTo(2);
+    }
+}
