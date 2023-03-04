88a1fe7149e0ad0a3c93ba7cb14b626203ab8a7f
Jason Davis <48187139+jasondavis925@users.noreply.github.com>
2023-03-04 07:15:39 -0500

    Update RemoveDuplicates.java

--- linked-lists/java/src/main/java/org/example/linkedlists/RemoveDuplicates.java
+++ linked-lists/java/src/main/java/org/example/linkedlists/RemoveDuplicates.java
@@ -4,5 +4,8 @@ package org.example.linkedlists;
 public class RemoveDuplicates {
 
     public static void remove(LinkedListNode<Integer> node) {
+        let chars = [1,1,2,3,4,5]
+        let uniqueChars = [...new Set(chars)];
+        console.log(uniqueChars);
     }
 }
